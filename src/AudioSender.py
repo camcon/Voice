fe5f58afe288d50f1149b1b8b571f6934a52a832
aconte21 <anthonydconte@lewisu.edu>
2017-03-25 19:42:08 -0500

    receiver and sender put into methods
    made a AudioReciever.startListenting method
    made a AudioSender.connect(host) method

--- src/AudioSender.py
+++ src/AudioSender.py
@@ -10,49 +10,51 @@ import time
 
 port = 8080
 host = '10.120.43.121'
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-try:
-    sock.connect((host, port))
-    print 'Connected to:', host
-except Exception as ex:
-    print ex
-    
-stream = StreamFactory.makeInputStream()
-outStream = StreamFactory.makeOutputStream()
-CHUNK = 1024
+def connect():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        print 'connecting to ', host
+        sock.connect((host, port))
+        print 'Connected to:', host
+    except Exception as ex:
+        print ex
+        
+    stream = StreamFactory.makeInputStream()
+    outStream = StreamFactory.makeOutputStream()
+    CHUNK = 1024
 
-startTime = time.time()
-endTime = startTime + 3
-message = ""
+    startTime = time.time()
+    endTime = startTime + 3
+    message = ""
 
-# Continuous audio stream:
-print 'starting audio stream'
-while True:
-    data = stream.read(CHUNK)
-    sock.send(data)
-print 'writing message'
+    # Continuous audio stream:
+    print 'starting audio stream'
+    while True:
+        data = stream.read(CHUNK)
+        sock.send(data)
+    print 'writing message'
 
-# everything below for timed messages:
-##while startTime < endTime:
-##    data = stream.read(CHUNK)
-##    #sock.send(data)
-##    message = message + data
-##    startTime = time.time()
-##    print str(endTime - startTime)
-##    #outStream.write(data)
-##    print 'writing message'
-#outStream.write(message)
-##try:
-##    while message:
-##        sent = sock.send(message)
-##        if not sent:
-##            break
-##        message = message[sent:]
-##    outStream.write(sent)
-##except Exception as ex:
-##    print ex
-#sock.send(message)    
-print "sent"
-#sock.close()
+    # everything below for timed messages:
+    ##while startTime < endTime:
+    ##    data = stream.read(CHUNK)
+    ##    #sock.send(data)
+    ##    message = message + data
+    ##    startTime = time.time()
+    ##    print str(endTime - startTime)
+    ##    #outStream.write(data)
+    ##    print 'writing message'
+    #outStream.write(message)
+    ##try:
+    ##    while message:
+    ##        sent = sock.send(message)
+    ##        if not sent:
+    ##            break
+    ##        message = message[sent:]
+    ##    outStream.write(sent)
+    ##except Exception as ex:
+    ##    print ex
+    #sock.send(message)    
+    print "sent"
+    #sock.close()
 
     
